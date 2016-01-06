34f439ac068f0a1e250c76525a22aec21190a228
asgsanchezgomez <asgsanchezgomez@gmail.com>
2016-01-06 12:48:26 +0100

    Primera pagina y Favicon

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. SOLO SE EJECUTA PARA TRANSACCIONES GET
+CON USE PODRIA RESPONDER A CUALQUIER PETICION (POST, GET, ETC)*/
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' });
 });
 
 module.exports = router;
