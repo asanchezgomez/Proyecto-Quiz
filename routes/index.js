0c49f9e8ff9cb9c9c50533c2fb6db52e58b52c3f
asgsanchezgomez <asgsanchezgomez@gmail.com>
2016-01-10 18:40:31 +0100

    Despliegue DB en Heroku con Autoload

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,12 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-//Llamamos a la funcion correspondiente del controlador del Quiz
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+//Autoload de comandos con :quizId
+router.param('quizId',quizController.load); //autoload :quizId
+
+//Definicion de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
